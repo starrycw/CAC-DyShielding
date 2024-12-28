953357123788fd00d79bd0994cd94b2e4fa414c6
starrycw <AronaLink@outlook.com>
2024-12-28 23:36:34 +0800

    SP20241228-2335

--- PythonProject/DesignGen/Gen_VH_FNSHeader.py
+++ PythonProject/DesignGen/Gen_VH_FNSHeader.py
@@ -3,7 +3,7 @@
 # Description: Generating the VH files for FNS/IFNS-CAC codec designs
 # Author: CWei
 # Verion: 20241228
-# Rexision:
+# Revision:
 ########################################################################################################################
 ########################################################################################################################
 
@@ -26,40 +26,41 @@ def genVH_FNS_HeaderFiles(codeword_maxLength: int, if_export_file=False, export_
     vhCodeLines_list = []
     note_timestring = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
     if export_filePath is None:
-        export_filePath = "exported_files/verilogCodeGen_ringCACCodec-{}.vh".format(note_timestring)
+        export_filePath = "exported_files/Header_FNS-{}.vh".format(note_timestring)
 
-    vhCodeLines_list.append("// verilogCodeGen_ringCACCodec [ver = 20240219-01] [Creation Time = " + note_timestring + ']\n')
-    vhCodeLines_list.append("// codeword_maxLength={}\n".format(codeword_maxLength))
-    vhCodeLines_list.append("// ------------------------------\n")
-    vhCodeLines_list.append("// ------------------------------\n")
-    vhCodeLines_list.append("`ifndef __FNSCATF_HEADER__\n")
-    vhCodeLines_list.append("    `define __FNSCATF_HEADER__\n")
+    vhCodeLines_list.append("/////////////////////////////////////////////////////////////////////////////////")
+    vhCodeLines_list.append("// ################################# DesignGen #################################\n")
+    vhCodeLines_list.append("// ######################## https://github.com/starrycw ########################")
+    vhCodeLines_list.append("// #############################################################################")
+    vhCodeLines_list.append("// ### Description: VH file for FNS-CAC codec designs")
+    vhCodeLines_list.append("// ### Script Version: 2024.12.28.01")
+    vhCodeLines_list.append("// ### Script Revision: ")
+    vhCodeLines_list.append("// ###  ")
+    vhCodeLines_list.append("// ### Creation Time: "+ note_timestring)
+    vhCodeLines_list.append("// ### Parameters: ")
+    vhCodeLines_list.append("// ###### codeword_maxLength = {}".format(codeword_maxLength))
+    vhCodeLines_list.append("// #############################################################################")
+    vhCodeLines_list.append("/////////////////////////////////////////////////////////////////////////////////")
+
+    vhCodeLines_list.append("`ifndef __HEADER_FNS__\n")
+    vhCodeLines_list.append("    `define __HEADER_FNS__\n")
     vhCodeLines_list.append("\n")
-    vhCodeLines_list.append("    // ------------------------------\n")
-    vhCodeLines_list.append("    // VH_FNSCATF_DataInBitWidth\n")
-    vhCodeLines_list.append("    // The value of VH_FNSCATF_DataInBitWidth_[i]bitCW is the max input data bitwidth of the encoder generating i-bit codeword. \n")
-    vhCodeLines_list.append("    // For example: input wire [`VH_FNSCATF_DataInBitWidth_7bitCW - 1 : 0] datain\n")
-
-    for cwLen_i in range(4, codeword_maxLength + 1):
-        CodecRingCAC_i = RingCAC_Alg.Ring2CTransCAC_Codec.Codec_Ring2CTransCAC_FNSBased(len_cw=cwLen_i)
-        maxDecValue_i = CodecRingCAC_i.getParam_maxInputLimitation()
-        maxInputBitWidth = math.floor(math.log2(maxDecValue_i))
-        vhCodeLines_list.append("    `define VH_FNSCATF_DataInBitWidth_{}bitCW {}\n".format(cwLen_i, maxInputBitWidth))
 
     vhCodeLines_list.append("\n")
-    vhCodeLines_list.append("    // ---------------------------------\n")
-    vhCodeLines_list.append("    // VH_FNSCATF_NSValue & VH_FNSCATF_NSValueMaxBinWidth\n")
-    vhCodeLines_list.append("    // The value of VH_FNSCATF_NSValue_Pn is the number of the n-bit ATF (not CATF) codewords.\n")
-    vhCodeLines_list.append("    // The value of VH_FNSCATF_NSValueMaxBinWidth_Pn is the bit-width of VH_FNSCATF_NSValue_P.\n")
-    nsValue_a = 1
-    nsValue_b = 1
-    for cwLen_k in range(1, codeword_maxLength + 1):
-        nsValue_c = nsValue_a + nsValue_b
-        vhCodeLines_list.append("    `define VH_FNSCATF_NSValue_P{} {}\n".format(cwLen_k, nsValue_c))
-        ns_valuec_binWidthCeil = math.ceil(math.log2(nsValue_c))
-        vhCodeLines_list.append("    `define VH_FNSCATF_NSValueMaxBinWidth_P{} {}\n".format(cwLen_k, ns_valuec_binWidthCeil))
-        nsValue_a = nsValue_b
-        nsValue_b = nsValue_c
+    vhCodeLines_list.append("    `define FNS_Value_1 1\n")
+    vhCodeLines_list.append("    `define FNS_DataBW_1 1\n")
+    vhCodeLines_list.append("    `define FNS_ResBW_1 1\n")
+
+    vhCodeLines_list.append("\n")
+    vhCodeLines_list.append("    `define FNS_Value_2 1\n")
+    vhCodeLines_list.append("    `define FNS_DataBW_2 1\n")
+    vhCodeLines_list.append("    `define FNS_ResBW_2 1\n")
+
+    for idx_i in range()
+
+
+
+
 
     vhCodeLines_list.append("\n")
     vhCodeLines_list.append("`endif\n")
